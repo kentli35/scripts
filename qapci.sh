5f7e4c8a78eacb962b8eefcb25a0b7ca468ad4e4
feomepler <lh@msn.cn>
2015-01-11 19:56:48 +0800

    UI manager works now

--- qapci.sh
+++ qapci.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #TEST
-NEXUS_USERNAME=SVNDeployment
-NEXUS_PASSWORD=ILoveECWise2013!
+USER=SVNDeployment
+PASSWORD=ILoveECWise2013!
 NEXUS_IP=192.168.1.199
-software=ui-manager-service
-
 
 detect_latest(){
 	if [ -f build.properties.snapshot ]
@@ -27,9 +25,10 @@ update_hosts(){
 	fi
 }
 
+#components can be: uimanager,virtualhost,avhmanager
 build_components(){
 	release_home=/release_home/$1
-	install_path=/usr/local/$1
+	install_path=/usr/local/perfectcomp/$1
 	if [ -d $release_home ]
 		then 
 			rm -rf $release_home
@@ -51,7 +50,8 @@ build_components(){
 	fi
 	
 	echo "Getting application.properties from svn..."
-	wget  --http-user=$NEXUS_UESRNAME --http-passwd=$NEXUS_PASSWORD --no-check-certificate \
+	wget  --quiet --http-user=$NEXUS_UESRNAME --http-passwd=$NEXUS_PASSWORD \
+	--no-check-certificate \
 	https://svn.ecwise.com/svn/perfectComp/perfectcomp-configurations/trunk/QA/$1\
 	/application.properties -O $release_home/config/application.properties
 
@@ -71,7 +71,8 @@ build_components(){
 		then
 			rm $servicefile
 	fi
-	wget  --http-user=$NEXUS_UESRNAME --http-passwd=$NEXUS_PASSWORD --no-check-certificate \
+	wget  --quiet --http-user=$NEXUS_UESRNAME --http-passwd=$NEXUS_PASSWORD \
+	--no-check-certificate \
 	https://svn.ecwise.com/svn/perfectComp/utility-scripts/service-control/trunk/pc-uimanager \
 	-O /etc/init.d/$software
 	chmod 755 /etc/init.d/$software
@@ -80,4 +81,131 @@ build_components(){
 	echo "$software is successfully deployed with version $package_version!"
 }
 
+build_uimanager(){
+	comp=uimanager
+	release_home=/release_home/$comp
+	install_path=/usr/local/perfectcomp/$comp
+	package_version=$1	
+	if [[ "$package_version" =~ "SNAP" || "$package_version" =~ "LATEST" ]] 
+		then 
+			snapshot_or_release='snapshots'
+			echo "You are deploying a SNAPSHOT version of UI Manager..."
+		else
+			snapshot_or_release='releases'
+			echo "You are deploying a RELEASE version of UI Manager..." 
+	fi
 
+	if [ -d $release_home ]
+		then 
+			rm -rf $release_home
+	fi
+	
+	mkdir -p $release_home/config && cd $release_home
+	
+	#Download the package from Nexus, here we use a while loop to check if the
+	#package is valid, if not, we'll download it again until the package file 
+	#is correct and ready to use.
+	err_count=0
+	while true
+	do
+		echo -n "Getting package from nexus..."
+		wget --quiet --http-user=$USER --http-password=$PASSWORD \
+		--no-check-certificate \
+		"https://nexus.ecwise.com/service/local/artifact/maven/redirect?r=${snapshot_or_release}&g=com.c2r.perfectcomp&a=ui-manager-service&v=$package_version&e=jar" \
+		--output-document=$comp.jar
+		
+		if [ ! -s $comp.jar ]
+			then 
+				echo "[FAILED]"
+				echo "$comp.jar is not a valid file, will try downloading again."
+				((err_count++))
+			else
+				echo "[OK]"
+				break
+		fi
+
+		if [[ $err_count -gt 3 ]]
+			then 
+				echo "Maximum error count exceeded, please try later."
+				exit 1
+		fi
+	done
+	
+	#Download application.properties from svn, the same loop as package.
+	err_count=0
+	while true
+	do
+		echo -n "Getting application.properties from svn..."
+		wget  --quiet --http-user=$USER --http-passwd=$PASSWORD \
+		--no-check-certificate \
+	https://svn.ecwise.com/svn/perfectComp/perfectcomp-configurations/trunk/Staging/ui-manager-service/application.properties \
+		-O $release_home/config/application.properties
+
+		if [ ! -s $release_home/config/application.properties ]
+			then 
+				echo "[FAILED]"
+				echo "application.properties is not a valid file, \
+				will try downloading again."
+				((err_count++))
+			else
+				echo "[OK]"
+				break
+		fi
+
+		if [[ $err_count -gt 3 ]]
+			then 
+				echo "Maximum error count exceeded, please try later."
+				exit 1
+		fi
+	done
+	
+
+	echo -n "Clearing old version components..."
+	service pc-$comp stop > /dev/null
+	if [ -d $install_path ] 
+		then 
+			rm -rf $install_path
+	fi
+	mkdir -p $install_path && echo "[OK]"
+
+	echo -n "Copying files to install path..."
+		cp -rp * $install_path && echo "[OK]"
+
+	#Download startup script from svn, the same loop as above.
+	err_count=0
+	while true
+	do 
+		echo -n "Getting startup script form svn..."
+		if [ -f /etc/init.d/pc-$comp ] 	
+			then
+				rm /etc/init.d/pc-$comp
+		fi
+		wget --quiet --http-user=$USER --http-passwd=$PASSWORD \
+		--no-check-certificate \
+		https://svn.ecwise.com/svn/perfectComp/utility-scripts/service-control/trunk/pc-$comp \
+		-O /etc/init.d/pc-$comp
+
+		if [ ! -s /etc/init.d/pc-$comp ]
+			then 
+				echo "[FAILED]"
+				echo "pc-$comp is not a valid file, \
+				will try downloading again."
+				((err_count++))
+			else
+				echo "[OK]"
+				break
+		fi
+
+		if [[ $err_count -gt 3 ]]
+			then 
+				echo "Maximum error count exceeded, please try later."
+				exit 1
+		fi
+	done
+
+	chmod 755 /etc/init.d/pc-$comp
+	service pc-$comp start
+	echo "UI Manager service is successfully deployed with version: $package_version!"
+}
+
+build_uimanager LATEST
